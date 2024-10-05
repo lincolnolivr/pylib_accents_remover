647f7108632426cbcc83473342138b504a55ed1d
Lincoln Oliver <lincolnolive@gmail.com>
2024-10-05 15:20:46 -0300

    Repository preparation for publishing

--- setup.py
+++ setup.py
@@ -1,44 +1,19 @@
-# %%
-convert_list = {
-    'à': {'replacer':'a'},
-    'ã': {'replacer':'a'},
-    'ä': {'replacer':'a'},
-    'â': {'replacer':'a'},
-    'á': {'replacer':'a'},
-    'è': {'replacer':'e'},
-    'é': {'replacer':'e'},
-    'ê': {'replacer':'e'},
-    'ë': {'replacer':'e'},
-    'í': {'replacer':'i'},
-    'ì': {'replacer':'i'},
-    'î': {'replacer':'i'},
-    'ï': {'replacer':'i'},
-    'ï': {'replacer':'i'},
-    'ò': {'replacer':'o'},
-    'ó': {'replacer':'o'},
-    'ô': {'replacer':'o'},
-    'õ': {'replacer':'o'},
-    'ö': {'replacer':'o'},
-    'ù': {'replacer':'u'},
-    'ú': {'replacer':'u'},
-    'û': {'replacer':'u'},
-    'ü': {'replacer':'u'},
-    'ç': {'replacer':'c'},
-    'ñ': {'replacer':'n'}}
-# %%
+from setuptools import setup
 
-def letter_replacer(letter):
-    if letter in convert_list:
-        return letter.replace(letter, convert_list[letter]['replacer'])
-    return letter
-
-def remove_accents(word: str):
-    for letter in word:
-        lower_letter = letter.lower()
-        new_letter = letter_replacer(lower_letter)
-
-        if letter.isupper():
-            new_letter = new_letter.upper()
-            
-        word = word.replace(letter, new_letter)
-    return word
+with open('README.md', 'r') as file:
+    readme = file.read()
+ 
+setup(
+    name='remove_accents',
+    version='0.1.0',
+    license='MIT License',
+    author='Lincoln Oliver',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author_email='lincolnolive@gmail.com',
+    keywords='remove accents',
+    description='A function to remove string accents withouth changing other characters',
+    packages=['src'],
+    install_requires=[],
+    url='https://github.com/lincolnolivr/remove_accents-pylib.git'
+)
